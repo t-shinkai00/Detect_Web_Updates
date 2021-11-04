45877bf8e431328831edcbf40797049fdf216f49
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-04 22:46:17 +0900

    read and write element file

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ soup=BeautifulSoup(res.text, "html.parser")
 # print(soup)
 
 new_elem=str(soup.select(".body"))
+
+try:
+  with open("old_elem.txt") as f:
+    old_elem=f.read()
+except IOError:
+  old_elem=""
+
+if old_elem==new_elem:
+  print("No change")
+else:
+  with open("old_elem.txt", "w") as f:
+    f.write(new_elem)
+  print("There are some change")
