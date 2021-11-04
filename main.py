270cdbb32bb6f55c869a48d658293c8554789ee3
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-04 22:36:30 +0900

    get html

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import requests
 from secrets import URL,USERNAME,PASSWORD
 
 res=requests.get(url=URL, auth=(USERNAME,PASSWORD))
-print(res)
+# print(res)
+
+soup=BeautifulSoup(res.text, "html.parser")
+# print(soup)
+
+new_elem=str(soup.select(".body"))
