1e2dfd9088e5dbc89d50ac44385a7e75c16c2dda
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-05 18:12:31 +0900

    remove bot's name

--- slackPost.py
+++ slackPost.py
@@ -5,13 +5,11 @@ def post(mode):
   if mode==1:
     data = json.dumps({
         'text': '研究室wikiの変更を検知しました。https://www2.nm.cs.uec.ac.jp/wiki/wiki.cgi?action=LIST',  #通知内容
-        'username': 'wiki変更検知Bot',  #ユーザー名
         'link_names': 1,  #名前をリンク化
     })
   if mode==0:
     data = json.dumps({
         'text': '研究室wikiの変更を検知しませんでした。',  #通知内容
-        'username': 'wiki変更検知Bot',  #ユーザー名
         'link_names': 1,  #名前をリンク化
     })
   requests.post(url=WEBHOOK_URL,data=data)
