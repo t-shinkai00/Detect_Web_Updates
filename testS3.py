67591147860337d5937f426e4c22f57eaf9fd721
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-06 20:59:52 +0900

    add writeS3 function

--- testS3.py
+++ testS3.py
@@ -1,16 +1,22 @@
 import boto3
 
+# バケット名,オブジェクト名
+BUCKET_NAME = 'old-elem-labwiki'
+OBJECT_KEY_NAME = 'old_elem.txt'
+s3 = boto3.resource('s3')
+
+bucket = s3.Bucket(BUCKET_NAME)
+obj = bucket.Object(OBJECT_KEY_NAME)
+
 def readS3():
-  # バケット名,オブジェクト名
-  BUCKET_NAME = 'old-elem-labwiki'
-  OBJECT_KEY_NAME = 'old_elem.txt'
-
-  s3 = boto3.resource('s3')
-
-  bucket = s3.Bucket(BUCKET_NAME)
-  obj = bucket.Object(OBJECT_KEY_NAME)
-
   response = obj.get()
   body = response['Body'].read()
-
   return body.decode('utf-8')
+
+def writeS3(input):
+  data=input.encode("utf-8")
+  obj.put(Body=bytearray(data))  # データを書き込む
+
+print(readS3())
+writeS3("こんにちわ")
+print(readS3())
