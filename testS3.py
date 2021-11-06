83fbceb7fb9b7fde21bc8891f637a53c50884859
t-shinkai00 <t.shinkai00@gmail.com>
2021-11-06 20:29:54 +0900

    get from S3

---
+++ testS3.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+# バケット名,オブジェクト名
+BUCKET_NAME = 'old-elem-labwiki'
+OBJECT_KEY_NAME = 'old_elem.txt'
+
+s3 = boto3.resource('s3')
+
+bucket = s3.Bucket(BUCKET_NAME)
+obj = bucket.Object(OBJECT_KEY_NAME)
+
+response = obj.get()
+body = response['Body'].read()
+
+print(body.decode('utf-8'))
