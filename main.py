0d7e93115dd7acf1a2b52aeec273c39139d09a7c
t-shinkai00 <t.shinkai00@gmail.com>
2021-12-13 04:59:49 +0900

    print status code of response

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def detect_updates():
   from ReadWriteS3 import readS3,writeS3
 
   res=requests.get(url=URL, auth=(USERNAME,PASSWORD))
-  # print(res)
+  print(res.status_code)
 
   soup=BeautifulSoup(res.text, "html.parser")
   # print(soup)
@@ -20,17 +20,19 @@ def detect_updates():
 
   if old_elem==new_elem:
     print("No change")
-    return False
   else:
     writeS3(new_elem)
-    print("There are some changes")
-    return True
+    if res.status_code==200:
+      print("There are some changes")
+      post(res.status_code)
+    else:
+      print(f"Error: {res.status_code}")
+      post(res.status_code)
 
 from slackPost import post
 
-def main():
-  if detect_updates():
-    post()
+def main(event, lambda_context):
+  detect_updates()
 
 if __name__ == "__main__":
-  main()
+  main(event, lambda_context)
--- slackPost.py
+++ slackPost.py
@@ -1,9 +1,16 @@
 import requests, json
 from secrets import WEBHOOK_URL
 
-def post():
-  data = json.dumps({
-      'text': '研究室wikiの変更を検知しました。https://www2.nm.cs.uec.ac.jp/wiki/wiki.cgi?action=LIST',  #通知内容
-      'link_names': 1,  #名前をリンク化
-  })
+def post(status_code):
+  if status_code ==200:
+    data = json.dumps({
+        'text': '研究室wikiの変更を検知しました。https://www2.nm.cs.uec.ac.jp/wiki/wiki.cgi?action=LIST',  #通知内容
+        'link_names': 1,  #名前をリンク化
+    })
+  else:
+    data = json.dumps({
+        'text': f'{status_code}番のレスポンスが返りました。サーバーが落ちている可能性があります。',  #通知内容
+        'link_names': 1,  #名前をリンク化
+    })
+  # print(data)
   requests.post(url=WEBHOOK_URL,data=data)
